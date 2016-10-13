ed25d0f659977af4d40e479b801458981ac4e04c
youran1024 <youran1024@163.com>
2016-10-13 16:56:49 +0800

    添加Travis Ci

--- Pods/Target Support Files/Pods-RedpacketDemo/Pods-RedpacketDemo-resources.sh
+++ Pods/Target Support Files/Pods-RedpacketDemo/Pods-RedpacketDemo-resources.sh
@@ -79,6 +79,14 @@ EOM
       ;;
   esac
 }
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "RedPacketAlipay/AlipaySDK.bundle"
+  install_resource "RedpacketLib/RedpacketStaticLib/resources/RedPacketResource.bundle"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "RedPacketAlipay/AlipaySDK.bundle"
+  install_resource "RedpacketLib/RedpacketStaticLib/resources/RedPacketResource.bundle"
+fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
